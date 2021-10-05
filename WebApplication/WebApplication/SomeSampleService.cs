a5bd3a4c4d881a61a9a85d552cfb5b4cc22b4139
alex <aleksey-romanov@outlook.com>
2021-10-05 23:28:52 +0300

    init

---
+++ WebApplication/WebApplication/SomeSampleService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication
+{
+    public class SomeSampleService : BackgroundService
+    {
+        private readonly ServiceIdProvider _serviceIdProvider;
+        private readonly ILogger<SomeSampleService> _logger;
+
+        public SomeSampleService(ILogger<SomeSampleService> logger, ServiceIdProvider serviceIdProvider)
+        {
+            _logger = logger;
+            _serviceIdProvider = serviceIdProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Server '{ServerId}' is running {ThreadId}",
+                    _serviceIdProvider.ServiceId.ToString("N"),
+                    Thread.CurrentThread.ManagedThreadId);
+
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
